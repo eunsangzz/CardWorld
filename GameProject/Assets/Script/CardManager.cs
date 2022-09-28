b083c6986a5c9397ab76ec05240e2ddc29bc91c3
eunsangzz <48229728+eunsangzz@users.noreply.github.com>
2022-09-28 13:33:52 +0900

    cardmanager
    cardmanager uimanager

---
+++ GameProject/Assets/Script/CardManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    //카드 구매버튼 눌렀을때 저장해둔 프리팹중에 랜덤으로 하나 생성 
+    //구매 버튼 업그레이드 적용해서 1단계 나무 돌 2단계 철 금 등등 으로 세팅
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GameProject/Assets/Script/UImanager.cs
+++ GameProject/Assets/Script/UImanager.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class UImanager : MonoBehaviour
 {
     public GameObject craftUi;
+    public GameObject craftUiBtn;
     public GameObject menuUi;
+    public GameObject menuUiBtn;
  
     public void CraftUiBtn()
     {
         craftUi.SetActive(true);
+        craftUiBtn.SetActive(false);
+    }
+    public void CraftUiCloseBtn()
+    {
+        craftUi.SetActive(false);
+        craftUiBtn.SetActive(true);
     }
 
-    public void MenuUibtn()
+    public void MenuUiBtn()
     {
         menuUi.SetActive(true);
+        menuUiBtn.SetActive(false);
     }
-
-    public void BuyBtn()
+    public void MenuUiCloseBtn()
     {
-        
+        menuUi.SetActive(false);
+        menuUiBtn.SetActive(true);
     }
 }
